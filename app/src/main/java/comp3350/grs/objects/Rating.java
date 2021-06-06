7c0c521e86a77f20d1b25710a48292d54310c8c3
kathrn01 <84199502+kathrn01@users.noreply.github.com>
2021-06-06 18:38:52 -0500

    Updated Rating
    can update the respective game's overall rating

--- app/src/main/java/comp3350/grs/objects/Rating.java
+++ app/src/main/java/comp3350/grs/objects/Rating.java
@@ -1,16 +1,21 @@
 package comp3350.grs.objects;
 
+import comp3350.grs.objects.Game;
+
 public class Rating
 {
     private int rating; //from one to five 
-    //potentially make a field with the associated Game object (?), so the new rating can directly update the overall game rating 
+    private Game belongsTo; //so the new rating can directly update the overall game rating -- if there is a better way to do this feel free to change it
 
-    public Rating(int ratingGiven)
+    public Rating(int ratingGiven, Game rated)
     {
         rating = 0; // default -- if no ratings (or no valid ratings) given yet
+        belongsTo = rated; 
+        
         if(ratingGiven > 0 && ratingGiven <= 5)
         {
             rating = ratingGiven;
+            belongsTo.addRating(this); //add this rating to the game 
         }
         else
         {
@@ -25,6 +30,6 @@ public class Rating
 
     public String toString()
     {
-        return "New Game Rating: "+rating+" out of 5 points.";
+        return "New rating for game "+belongsTo.getName()+": "+rating+" out of 5 points.";
     }
 }

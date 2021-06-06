b29da25275b7da760d1d9e9fab0dbe1b0fb2ddb2
kathrn01 <84199502+kathrn01@users.noreply.github.com>
2021-06-06 18:12:19 -0500

    Updated AccessRatings *incomplete

--- app/src/main/java/comp3350/grs/business/AccessRatings.java
+++ app/src/main/java/comp3350/grs/business/AccessRatings.java
@@ -10,42 +10,43 @@ import java.util.List;
 import java.util.ArrayList; 
 
 import comp3350.grs.objects.Rating;
-//add imports for Main, Services, and Database once implemented 
+import comp3350.grs.objects.Game;
+import comp3350.grs.persistence.DataAccessStub; 
 
 
 public class AccessRatings
 {
-    //private DataAccessStub dataAccess; // stub database (name taken from example file)
+    private DataAccessStub dataAccess; // stub database (name taken from example file)
     private List <Rating> ratings; 
-    private Rating rating; 
-    private int currRating; 
+    private Rating currRating; 
+    private int currRatingIndex; 
 
     public AccessRatings()
     {
-        //dataAccess = (DataAccessStub) Services.getDataAccess(Main.dbName);
+        dataAccess = (DataAccessStub) Services.getDataAccess(Main.dbName);
         ratings = null; 
         rating = null; 
         currRating = 0; 
     }
 
-    public String getRatings(List <Rating> ratings)
+    public String getRatings(Game game)
     {
-        //ratings.clear(); --- this was in the example, but why clear it ?
-        return " "; // actually return dataAccess.getRatingSequential(ratings); once database implemented
+        ratings.clear();
+        return dataAccess.getAllRatings(game); //returns the ratings for a specific game provided as parameter 
     }
 
-    public Rating getSequential()
+    public Rating getSequential(Game game)
     {
-        //String result = null; 
+        String result = null; 
         if(ratings == null)
         {
             ratings = new ArrayList<Rating>();
-            //result = dataAccess.getRatingSequential(ratings);
-            currRating = 0; 
+            result = dataAccess.getRatingSequential(game);
+            currRatingIndex = 0; 
         }
-        if(currRating < ratings.size())
+        if(currRatingIndex < ratings.size())
         {
-            rating = (Rating) ratings.get(currRating);
+            currRating = (Rating) ratings.get(currRating);
             currRating++; 
         }
         else
@@ -57,19 +58,19 @@ public class AccessRatings
         return rating; 
     }
 
-    //change these three below to String return type once import Database class
+   
     public void insertRating(Rating currentRating)
     {
-        //return dataAccess.insertRating(currentRating);
+        return dataAccess.insertRating(currentRating);
     }
 
     public void updateRating(Rating currentRating)
     {
-        //return dataAccess.updateRating(currentRating);
+        return dataAccess.updateRating(currentRating);
     }
 
     public void deleteRating(Rating currentRating)
     {
-        //return dataAccess.deleteRating(currentRating);
+        return dataAccess.deleteRating(currentRating);
     }
 }

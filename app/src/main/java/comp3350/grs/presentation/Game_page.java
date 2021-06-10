cc5944a8483ef27482b3a93010a205d6c8ed12cc
Shiqing <gsq7430@163.com>
2021-06-10 17:29:37 -0500

    xml for game page

---
+++ app/src/main/java/comp3350/grs/presentation/Game_page.java
@@ -0,0 +1,16 @@
+package comp3350.grs.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import comp3350.grs.R;
+
+public class Game_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_page);
+    }
+}

6881f17fdb214e43ccedcbde1230f4637818a987
Shiqing <gsq7430@163.com>
2021-06-10 17:30:11 -0500

    xml for game page

---
+++ app/src/test/java/comp3350/grs/persistence/DataAccessStubTest.java
@@ -0,0 +1,35 @@
+package comp3350.grs.persistence;
+
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import comp3350.grs.objects.Game;
+import comp3350.grs.persistence.DataAccessStub;
+
+public class DataAccessStubTest {
+    DataAccessStub db;
+
+    @Before
+    public void initialization(){
+        db=new DataAccessStub("testDb");
+    }
+
+    @Test
+    public void testOpen(){
+        DataAccessStub db;
+        List<Game> games;
+        Game game;
+        db=new DataAccessStub("testDb");
+        db.open("testDb");
+        games= db.getAllGames();
+        for (int i = 0; i < games.size(); i++) {
+            game=games.get(i);
+            System.out.println(game);
+        }
+    }
+}

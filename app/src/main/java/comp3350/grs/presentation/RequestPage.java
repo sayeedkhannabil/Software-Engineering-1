0dd2ab2f93101c09255badcb16181f78345a21bd
Shiqing <gsq7430@163.com>
2021-07-17 14:16:44 -0500

    RequestPage

---
+++ app/src/main/java/comp3350/grs/presentation/RequestPage.java
@@ -0,0 +1,16 @@
+package comp3350.grs.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import comp3350.grs.R;
+
+public class RequestPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_page);
+    }
+}

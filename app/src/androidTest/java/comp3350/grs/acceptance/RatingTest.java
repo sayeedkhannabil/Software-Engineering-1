352390d581a4e47582f456ef727f9b4d387d61ac
hasinishrak <32551736+hasinishrak@users.noreply.github.com>
2021-07-27 11:55:01 -0500

    Update RatingTest.java
    acceptance test for ratings.

--- app/src/androidTest/java/comp3350/grs/acceptance/RatingTest.java
+++ app/src/androidTest/java/comp3350/grs/acceptance/RatingTest.java
@@ -1,5 +1,6 @@
 package comp3350.grs.acceptance;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.filters.MediumTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -8,6 +9,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import comp3350.grs.R;
 import comp3350.grs.presentation.MainActivity;
 import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
@@ -27,6 +29,44 @@ public class RatingTest {
 
     @Test
     public void testRating() {
+
+        onView(withText("CONTINUE AS GUEST")).check(matches(isDisplayed()));
+        onView(withText("CONTINUE AS GUEST")).perform(click());
+        onView(withText("Enter Game Gallery")).check(matches(isDisplayed()));
+        onView(withText("Enter Game Gallery")).perform(click());
+        onView(withText("Game Gallery")).check(matches(isDisplayed()));
+
+        onView(withText("7 Days to Die")).check(matches(isDisplayed()));
+        onView(withText("7 Days to Die")).perform(click());
+        onView(withText("7 Days to Die")).check(matches(isDisplayed()));
+
+        onView(withId(R.id.ratingBar)).perform(click());
+        onView(withId(R.id.ratingBar)).check(matches(isDisplayed()));
+
+        Espresso.pressBack();
+
+        onView(withText("Game Gallery")).check(matches(isDisplayed()));
+        onView(withText("7 Days to Die")).check(matches(isDisplayed()));
+        onView(withText("7 Days to Die")).perform(click());
+        onView(withText("7 Days to Die")).check(matches(isDisplayed()));
+        onView(withId(R.id.ratingBar)).check(matches(isDisplayed()));
+
+        Espresso.pressBack();
+
+        onView(withText("Arma 3")).check(matches(isDisplayed()));
+        onView(withText("Arma 3")).perform(click());
+        onView(withText("Arma 3")).check(matches(isDisplayed()));
+
+        onView(withId(R.id.ratingBar)).perform(click());
+        onView(withId(R.id.ratingBar)).check(matches(isDisplayed()));
+
+        Espresso.pressBack();
+
+        onView(withText("Game Gallery")).check(matches(isDisplayed()));
+        onView(withText("Arma 3")).check(matches(isDisplayed()));
+        onView(withText("Arma 3")).perform(click());
+        onView(withText("Arma 3")).check(matches(isDisplayed()));
+        onView(withId(R.id.ratingBar)).check(matches(isDisplayed()));
 
     }
 

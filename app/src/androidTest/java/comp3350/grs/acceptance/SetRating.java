e7d791d4dd4df67fb313a4912a4c5a5c6b725481
Shiqing <gsq7430@163.com>
2021-07-27 15:19:50 -0500

    Set Rating

---
+++ app/src/androidTest/java/comp3350/grs/acceptance/SetRating.java
@@ -0,0 +1,62 @@
+package comp3350.grs.acceptance;
+
+import android.view.InputDevice;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RatingBar;
+
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.CoordinatesProvider;
+import androidx.test.espresso.action.GeneralClickAction;
+import androidx.test.espresso.action.Press;
+import androidx.test.espresso.action.Tap;
+import androidx.test.espresso.matcher.ViewMatchers;
+
+import org.hamcrest.Matcher;
+
+public final class SetRating implements ViewAction {
+    private float ratingValue;
+
+    public SetRating(float ratingValue){
+        this.ratingValue=ratingValue;
+    }
+
+    @Override
+    public Matcher<View> getConstraints() {
+        Matcher <View> isRatingBarConstraint = ViewMatchers.isAssignableFrom(RatingBar.class);
+        return isRatingBarConstraint;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Custom view action to set rating.";
+    }
+
+    @Override
+    public void perform(UiController uiController, View view) {
+        GeneralClickAction viewAction = new GeneralClickAction(
+                Tap.SINGLE,
+                new CoordinatesProvider() {
+                    @Override
+                    public float[] calculateCoordinates(View view) {
+                        int[] locationOnScreen = new int[2];
+                        view.getLocationOnScreen(locationOnScreen);
+                        int screenX = locationOnScreen[0];
+                        int screenY = locationOnScreen[1];
+                        int numStars = ((RatingBar) view).getNumStars();
+                        float widthPerStar = 1f * view.getWidth() / numStars;
+                        float percent = ratingValue / numStars;
+                        float x = screenX + view.getWidth() * percent;
+                        float y = screenY + view.getHeight() * 0.5f;
+                        return new float[]{x - widthPerStar * 0.5f, y};
+                    }
+                },
+                Press.FINGER,
+                InputDevice.SOURCE_UNKNOWN,
+                MotionEvent.BUTTON_PRIMARY
+        );
+        viewAction.perform(uiController, view);
+    }
+}
+
